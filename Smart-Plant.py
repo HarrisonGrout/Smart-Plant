154629476badac8ebafbae72602ba83610ff348f
Harrison <23077163@student.uwa.edu.au>
2021-08-12 12:53:19 +0800

    Update Smart-Plant.py

--- Smart-Plant.py
+++ Smart-Plant.py
@@ -4,28 +4,81 @@ import os
 
 dirName = os.path.dirname(os.path.abspath(__file__))
 
-print("Welcome to the smart plant system developed by Harrison Grout\n")
-print("Please follow the prompts to enable certain settings:")
 
-ans = input("\tIs there an OLED attached: (Y/N): ")
-if (ans == "Y") | (ans == "y"):
-	print("\tOLED attached")
-	oledflag = True
-elif (ans == "N") | (ans == "n"):
-	print("\tNo OLED attached")
-	oledflag = False
-else:
-	print("\t!!!Invalid input, assuming no OLED!!!")
-	oledflag = False
+def init():
+	print("Welcome to the smart plant system developed by Harrison Grout\n")
+	print("Please follow the prompts to enable certain settings:")
 
-ans = input("\tIs there a pump attached: (Y/N): ")
-if (ans == "Y") | (ans == "y"):
-	print("\tPump attached")
-	pumpflag = True
-elif (ans == "N") | (ans == "n"):
-	print("\tNo pump attached")
-	pumpflag = False
-else:
-	print("\t!!!Invalid input, assuming no pump!!!")
-	pumpflag = False
 
+	#Input parsing to determine usage. Could be command line instead.
+
+	ans = input("\tIs there an OLED attached: (Y/N): ")
+	if (ans == "Y") | (ans == "y"):
+		print("\tOLED attached")
+		oledflag = True
+	elif (ans == "N") | (ans == "n"):
+		print("\tNo OLED attached")
+		oledflag = False
+	else:
+		print("\t!!!Invalid input, assuming no OLED!!!")
+		oledflag = False
+
+	ans = input("\tIs there a pump attached: (Y/N): ")
+	if (ans == "Y") | (ans == "y"):
+		print("\tPump attached")
+		pumpflag = True
+	elif (ans == "N") | (ans == "n"):
+		print("\tNo pump attached")
+		pumpflag = False
+	else:
+		print("\t!!!Invalid input, assuming no pump!!!")
+		pumpflag = False
+
+	ans = input("\tDo you want to connect Losant: (Y/N): ")
+	if (ans == "Y") | (ans == "y"):
+		print("\tLosant in use")
+		losantflag = True
+
+		# elif opt in ("-l", "--losant"):
+		# 	import losantHelper
+		# 	LOSANT_CLOUD = True
+		# 	filepath = '/'.join([dirName, arg])
+		# 	if os.path.abspath(arg):
+		# 		filepath = arg
+		# 	with open( filepath ) as f:
+		# 		try:
+		# 			losantConfig = json.load(f)
+		# 		except:
+		# 			print("ERROR: expecting JSON file")
+		# 			sys.exit()
+		# 		if not losantHelper.isConfigValid(losantConfig):
+		# 			sys.exit()
+
+
+
+	elif (ans == "N") | (ans == "n"):
+		print("\tLosant not in use")
+		losantflag = False
+	else:
+		print("\t!!!Invalid input, ignoring Losant!!!")
+		losantflag = False	
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	init()
+
+
+
+if __name__ == "__main__":
+	main()
