3123ec1963bb64e4c6835b62d497494b283d9aac
Harrison <23077163@student.uwa.edu.au>
2021-08-19 13:19:47 +0800

    Update pump_class.py

--- pump_class.py
+++ pump_class.py
@@ -18,10 +18,10 @@ class Pump(object):
 		if this.initialized:
 			if state == "idle":
 				print("set channel idle")
-				relayExp.setChannel(RELAY_EXP_ADDR, RELAY_EXP_CHANNEL, 0)
+				relayExp.setChannel(RELAYADDR, RELAYCHANNEL, 0)
 			elif state == "pump":
 				print("set channel pump")
-				relayExp.setChannel(RELAY_EXP_ADDR, RELAY_EXP_CHANNEL, 1)
+				relayExp.setChannel(RELAYADDR, RELAYCHANNEL, 1)
 			else:
 				print("INVALID PUMP STATE! Not pumping")
 		else:
