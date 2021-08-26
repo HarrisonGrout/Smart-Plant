ba71369ed1963f813f803a447ffe3891eac591a0
Harrison <23077163@student.uwa.edu.au>
2021-08-26 12:07:27 +0800

    Update Smart-Plant.py

--- Smart-Plant.py
+++ Smart-Plant.py
@@ -12,44 +12,50 @@ print("Welcome to the smart plant system developed by Harrison Grout\n")
 print("Please follow the prompts to enable certain settings:")
 
 
-PUMPTIME = 3
+PUMPTIME = 5
 UPDATETIME = 1
 
 
 #Input parsing to determine usage. Could be command line instead.
 
-ans = input("\tIs there an OLED attached: (Y/N): ")
-if (ans == "Y") | (ans == "y"):
-	print("\tOLED attached")
+MANUALINPUTS = False
+if MANUALINPUTS:
+	ans = input("\tIs there an OLED attached: (Y/N): ")
+	if (ans == "Y") | (ans == "y"):
+		print("\tOLED attached")
+		oledflag = True
+	elif (ans == "N") | (ans == "n"):
+		print("\tNo OLED attached")
+		oledflag = False
+	else:
+		print("\t!!!Invalid input, assuming no OLED!!!")
+		oledflag = False
+
+	ans = input("\tIs there a pump attached: (Y/N): ")
+	if (ans == "Y") | (ans == "y"):
+		print("\tPump attached")
+		pumpflag = True
+	elif (ans == "N") | (ans == "n"):
+		print("\tNo pump attached")
+		pumpflag = False
+	else:
+		print("\t!!!Invalid input, assuming no pump!!!")
+		pumpflag = False
+
+	ans = input("\tDo you want to connect Losant: (Y/N): ")
+	if (ans == "Y") | (ans == "y"):
+		print("\tLosant in use")
+		losantflag = True
+	elif (ans == "N") | (ans == "n"):
+		print("\tLosant not in use")
+		losantflag = False
+	else:
+		print("\t!!!Invalid input, ignoring Losant!!!")
+		losantflag = False	
+else:
 	oledflag = True
-elif (ans == "N") | (ans == "n"):
-	print("\tNo OLED attached")
-	oledflag = False
-else:
-	print("\t!!!Invalid input, assuming no OLED!!!")
-	oledflag = False
-
-ans = input("\tIs there a pump attached: (Y/N): ")
-if (ans == "Y") | (ans == "y"):
-	print("\tPump attached")
 	pumpflag = True
-elif (ans == "N") | (ans == "n"):
-	print("\tNo pump attached")
-	pumpflag = False
-else:
-	print("\t!!!Invalid input, assuming no pump!!!")
-	pumpflag = False
-
-ans = input("\tDo you want to connect Losant: (Y/N): ")
-if (ans == "Y") | (ans == "y"):
-	print("\tLosant in use")
 	losantflag = True
-elif (ans == "N") | (ans == "n"):
-	print("\tLosant not in use")
-	losantflag = False
-else:
-	print("\t!!!Invalid input, ignoring Losant!!!")
-	losantflag = False	
 
 serialPort = serial.Serial('/dev/ttyS1', 9600, timeout=2)
 if serialPort.isOpen() == False:
